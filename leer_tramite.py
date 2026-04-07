bece5b321f201877946941250e5a6efc6d457558
fpineda-esz <fpineda@esz.com.mx>
2026-04-07 10:57:04 -0600

    Delete leer_tramite.py

--- leer_tramite.py
+++
@@ -1,5 +0,0 @@
-# Abrimos el archivo en modo lectura ('r') y le indicamos que use 'utf-8' para no tener problemas con los acentos.
-with open('tramite_licencia.txt', 'r', encoding='utf-8') as archivo:
-    contenido = archivo.read()
-    print("--- LEYENDO BASE DE DATOS DEL MUNICIPIO ---")
-    print(contenido)
